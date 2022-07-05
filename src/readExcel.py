86f09c3edc3b94385a66052dad9f1c732b468c5c
Jayaprakash Thatiparthi <jnarayan@jnarayan-wsm1.internal.salesforce.com>
2022-07-05 15:20:28 -0500

    Fixed JSON

--- src/readExcel.py
+++ src/readExcel.py
@@ -5,19 +5,21 @@ df = pd.ExcelFile('./src/caseData.xlsx').parse('Sheet1') #you could add index_co
 caseIdList=[]
 caseIdList.append(df['CaseId'].values[...])
 print(caseIdList);
-pLoad = "{'incidents': ["
+pLoad = '{"incidents": ['
 for caseId in caseIdList[0]:
-    pLoad=pLoad + "{'id':" +str(caseId)+ "'status': 'closed'},"
+    pLoad=pLoad + '{"id":"' +str(caseId)+ '","type": "incident_reference","status": "resolved"}'
+    if caseIdList[0][-1] != caseId:
+        pLoad=pLoad + ','
     #pLoad = pLoad + str(caseId) + ','
-pLoad = pLoad + "]}"
+pLoad = pLoad + ']}'
 print(pLoad);
 conn = http.client.HTTPSConnection("api.pagerduty.com")
 payload = pLoad 
 headers = {
     'Content-Type': "application/json",
     'Accept': "application/vnd.pagerduty+json;version=2",
-    'From': "",
-    'Authorization': "Token token="
+    'From': "jnarayan@salesforce.com",
+    'Authorization': "Token token=u+Cx2dQFumdMPTAaAbUg"
     }
 conn.request("PUT", "/incidents", payload, headers)
 res = conn.getresponse()
