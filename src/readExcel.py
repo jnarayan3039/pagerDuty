420522620b231940c990eb8d523081099e85001e
Jayaprakash Thatiparthi <jnarayan@jnarayan-wsm1.internal.salesforce.com>
2022-07-06 14:47:33 -0500

    Code for Mass Updates

--- src/readExcel.py
+++ src/readExcel.py
@@ -5,23 +5,36 @@ df = pd.ExcelFile('./src/caseData.xlsx').parse('Sheet1') #you could add index_co
 caseIdList=[]
 caseIdList.append(df['CaseId'].values[...])
 print(caseIdList);
-pLoad = '{"incidents": ['
-for caseId in caseIdList[0]:
-    pLoad=pLoad + '{"id":"' +str(caseId)+ '","type": "incident_reference","status": "resolved"}'
-    if caseIdList[0][-1] != caseId:
-        pLoad=pLoad + ','
-    #pLoad = pLoad + str(caseId) + ','
-pLoad = pLoad + ']}'
-print(pLoad);
-conn = http.client.HTTPSConnection("api.pagerduty.com")
-payload = pLoad 
-headers = {
-    'Content-Type': "application/json",
-    'Accept': "application/vnd.pagerduty+json;version=2",
-    'From': "jnarayan@salesforce.com",
-    'Authorization': "Token token=u+Cx2dQFumdMPTAaAbUg"
-    }
-conn.request("PUT", "/incidents", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+print(abs(int(len(caseIdList[0])/3)))
+start = 0;
+end = 3;
+multiplier = 0;
+for count in range(0,int(len(caseIdList[0])/3)+1, 1):
+    pLoad = '{"incidents": ['
+    #start = int(len(caseIdList[0])/250)
+    if end > int(len(caseIdList[0])):
+        end = int(len(caseIdList[0]))
+    multipler = multiplier+1;
+    print(start, end);
+    for caseId in range(start, end, 1):
+        pLoad=pLoad + '{"id":"' +str(caseIdList[0][caseId])+ '","type": "incident_reference","status": "resolved"}'
+        if caseIdList[0][end-1] != caseIdList[0][caseId]:
+            pLoad=pLoad + ','
+            #pLoad = pLoad + str(caseId) + ','
+    pLoad = pLoad + ']}'
+    print(pLoad);
+    conn = http.client.HTTPSConnection("api.pagerduty.com")
+    payload = pLoad 
+    headers = {
+        'Content-Type': "application/json",
+        'Accept': "application/vnd.pagerduty+json;version=2",
+        'From': "jnarayan@salesforce.com",
+        'Authorization': "Token token=u+Cx2dQFumdMPTAaAbUg"
+        }
+    conn.request("PUT", "/incidents", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+    start = end
+    end = end+3
+    
